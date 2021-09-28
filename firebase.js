748cef9e7353608f998e84234196a19a68806bb3
Kenneth Ossai <kenossai@ymail.com>
2021-09-28 14:10:34 +0100

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBpD3ZfRqUnZGgJyIU7INBQ6ZslUrOsslg",
+
+    authDomain: "clone2-4611a.firebaseapp.com",
+
+    projectId: "clone2-4611a",
+
+    storageBucket: "clone2-4611a.appspot.com",
+
+    messagingSenderId: "236424251813",
+
+    appId: "1:236424251813:web:15b68e386aa4f69988404c",
+
+    measurementId: "G-GCDV66GLPP"
+
+  };
